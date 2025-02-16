1bbf489b715f34d330f81a71dae6daf35d010cbc
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2025-02-16 14:04:42 +0530

    Backend basic setup complete

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,8 +19,4 @@ app.use(express.json({limit : "16kb"}));
 app.use(express.urlencoded({extended:true, limit: "16kb"}));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.send("Hello users to Seva Sathi");
-});
-
 export default app;
