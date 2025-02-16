84180386ca0f4265e4c05eeb0a540536f8bca7e6
invtfl-yuvraj <yuvrajsingh3715@gmail.com>
2025-02-16 13:53:37 +0530

    Create Database connect using MongoDB atlas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,16 @@
-import http from "http";  
-import app from "./app.js"; 
+import http from "http";
+import app from "./app.js";
+import connectDB from "./db/db.js";
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+connectDB()
+  .then(() => {
+    server.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection error", err);
+  });
