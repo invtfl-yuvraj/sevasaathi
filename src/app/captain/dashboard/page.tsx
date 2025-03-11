baf9c81c12816b3ec0594c7cb5c4c12d1f2b55d5
Bharti Jayprakash <bhartijayprakash19@gmail.com>
2025-03-11 23:52:23 +0530

    Successfully created the captain Dashboard

--- src/app/captain/dashboard/page.tsx
+++ src/app/captain/dashboard/page.tsx
@@ -1,14 +1,16 @@
 import ButtonNavigation from "@/components/ButtonNavigation";
 import CaptainHeader from "@/components/CaptainHeader";
 import React from "react";
+import CaptainDashboard from "@/components/CaptainDashboard";
 
 function page() {
   return (
     <div className="h-screen w-full bg-gray-100 py-4 px-4">
       <CaptainHeader />
-      <div className="py-6">
-        <h1 className="font-bold text-3xl">Good Morning!🌞</h1>
-      </div>
+
+      <h1 className="font-bold text-3xl tracking-wide py-6">Good Morning!🌞</h1>
+
+      <CaptainDashboard />
       <div className="fixed h-16 w-full bottom-0 left-0 right-0 z-10">
         <ButtonNavigation />
       </div>
--- src/components/CaptainDashboard.tsx
+++ src/components/CaptainDashboard.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 function CaptainDashboard() {
   return (
-    <div>CaptainDashboard</div>
-  )
+    <div>
+      <div className="flex justify-center items-center flex-col">
+        <Image
+          src="/Icon/upcoming_order.png"
+          height={80}
+          width={80}
+          alt="No order Image"
+          className="bg-cover"
+        ></Image>
+        <h2 className="font-bold text-xl">No Upcoming Order</h2>
+        <p className="text-sm px-4 text-gray-500">
+          Currently you don't have any upcoming order. We will notify you for
+          orders near you.
+        </p>
+
+      </div>
+        <Link href="/captain/orders">
+            <button className="w-full h-full py-2 bg-[#6759FF] text-white border-none rounded-lg placeholder:text-base flex flex-col justify-center items-center text-base mb-5 font-bold">
+              Start Activity
+            </button>
+        </Link>
+    </div>
+  );
 }
 
-export default CaptainDashboard
+export default CaptainDashboard;
