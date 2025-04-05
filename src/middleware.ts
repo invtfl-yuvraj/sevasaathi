5ebe03d83c7d7fb40b9699d61b47b12c2069096b
Bharti Jayprakash <bhartijayprakash19@gmail.com>
2025-04-05 22:44:12 +0530

    Authentication Provider added for login

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req });
+  const url = req.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/user/login") ||
+      url.pathname.startsWith("/user/signup"))
+  ) {
+    return NextResponse.redirect(new URL("/user/dashboard", req.url));
+  }
+
+  if (!token && url.pathname.startsWith("/user")) {
+    return NextResponse.redirect(new URL("/user/login", req.url));
+  }
+
+  return NextResponse.redirect(new URL("/user/login", req.url));
+}
